eb36b96864bfcb1bcf57b8ffdd49e9bb1fc508c6
Pedro Gordillo <pedrogordillo2510@gmail.com>
2021-06-23 01:46:01 -0600

    creé el archivo Personaje.java

---
+++ src/personaje/Personaje.java
@@ -0,0 +1,31 @@
+public class Personaje{
+
+	protected int vida;
+	protected int ataqueMaximo;
+	protected String nombre;
+	protected int armadura;
+	protected boolean bestia;
+
+
+
+	public Personaje(String nombre, int vida, int ataqueMaximo, int armadura, boolean bestia){
+
+		this.nombre = nombre;
+		this.vida = vida;
+		this.ataqueMaximo = ataqueMaximo;
+		this.armadura = armadura;
+		this.bestia = bestia;
+
+	}
+
+	public void presentarse(){
+
+		System.out.println("Mi nombre es: " + nombre + " tengo " + vida + " de vida y " + armadura + " de armadura");
+
+
+	}
+
+
+
+
+}
